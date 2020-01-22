fd910349280b035461948f842fc01314b9385041
Unknown <unknown@example.com>
2020-01-22 11:13:47 +0530

    second activity added

--- app/src/main/java/in/ed/poonacollege/androidassignments/MainActivity.java
+++ app/src/main/java/in/ed/poonacollege/androidassignments/MainActivity.java
@@ -1,5 +1,6 @@
 package in.ed.poonacollege.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        startActivity(new Intent(this, SecondActivity.class));
     }
 
     @Override
